aace686a654cc6c15d88621fc796e70268d3fb23
kedareshubham11 <kedareshubham11@gmail.com>
2020-04-14 22:21:51 +0530

    Added Search Functionality

--- my_app/views.py
+++ my_app/views.py
@@ -1,14 +1,48 @@
+import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from . import models
 
-
+BASE_CGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url = BASE_CGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup =BeautifulSoup(data, features='html.parser')
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_titles = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image)
+        else:
+            post_image_url = 'https://previews.123rf.com/images/urfandadashov/urfandadashov1809/urfandadashov180901275/109135379-photo-not-available-vector-icon-isolated-on-transparent-background-photo-not-available-logo-concept.jpg'
+            print(post_image_url)
+
+        final_postings.append((post_titles, post_url, post_price, post_image_url))
+    
+
     stuff_for_front_end = {
-        'search' : search
+        'search' : search,
+        'final_postings': final_postings
     }
     return render(request, 'my_app/new_search.html', stuff_for_front_end)
