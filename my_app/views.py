4d46b82bf16038f8b128030e94feb0d3e8ea267b
Shubham <kedareshubham11#gmail.com>
2020-04-28 12:36:26 +0530

    Handled exceptions

--- my_app/views.py
+++ my_app/views.py
@@ -14,35 +14,47 @@ def new_search(request):
     search = request.POST.get('search')
     models.Search.objects.create(search=search)
     final_url = BASE_CGSLIST_URL.format(quote_plus(search))
-    response = requests.get(final_url)
-    data = response.text
-    soup =BeautifulSoup(data, features='html.parser')
-
-    post_listings = soup.find_all('li', {'class': 'result-row'})
-
-    final_postings = []
-
-    for post in post_listings:
-        post_titles = post.find(class_='result-title').text
-        post_url = post.find('a').get('href')
-        
-        if post.find(class_='result-price'):
-            post_price = post.find(class_='result-price').text
-        else:
-            post_price = 'N/A'
-
-        if post.find(class_='result-image').get('data-ids'):
-            post_image = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
-            post_image_url = BASE_IMAGE_URL.format(post_image)
-        else:
-            post_image_url = 'https://previews.123rf.com/images/urfandadashov/urfandadashov1809/urfandadashov180901275/109135379-photo-not-available-vector-icon-isolated-on-transparent-background-photo-not-available-logo-concept.jpg'
-            print(post_image_url)
-
-        final_postings.append((post_titles, post_url, post_price, post_image_url))
+    no_res = ''
     
+    try:
+        response = requests.get(final_url)
+        data = response.text
+        soup =BeautifulSoup(data, features='html.parser')
 
-    stuff_for_front_end = {
-        'search' : search,
-        'final_postings': final_postings
-    }
+        post_listings = soup.find_all('li', {'class': 'result-row'})
+        
+        if soup.find_all('div', {'class': 'alert alert-sm alert-warning'}):
+            no_result = soup.find_all('div', {'class': 'alert alert-sm alert-warning'})    
+            no_res = "No result found For '{}' search. (All words must match)".format(search)
+            print(no_result, ' ', no_res)
+
+        final_postings = []
+
+        for post in post_listings:
+            post_titles = post.find(class_='result-title').text
+            post_url = post.find('a').get('href')
+            
+            if post.find(class_='result-price'):
+                post_price = post.find(class_='result-price').text
+            else:
+                post_price = 'N/A'
+
+            if post.find(class_='result-image').get('data-ids'):
+                post_image = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+                post_image_url = BASE_IMAGE_URL.format(post_image)
+            else:
+                post_image_url = 'https://previews.123rf.com/images/urfandadashov/urfandadashov1809/urfandadashov180901275/109135379-photo-not-available-vector-icon-isolated-on-transparent-background-photo-not-available-logo-concept.jpg'
+                print(post_image_url)
+
+            final_postings.append((post_titles, post_url, post_price, post_image_url))
+        
+
+        stuff_for_front_end = {
+            'search' : search,
+            'final_postings': final_postings,
+            'no_res' :no_res,
+        }
+    except:
+        stuff_for_front_end = { 'errormessage' : 'Check Your Internet Connection'}
+
     return render(request, 'my_app/new_search.html', stuff_for_front_end)
